35fa4c221984db8bdc7fbd9e47961e1b127c4f7f
Ramon Medeiro <r.medeiro10@gmail.com>
2023-03-07 14:56:12 -0300

    Inserindo operador filesystem

--- dags/exemplo1.py
+++ dags/exemplo1.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.sensors.filesystem import FileSensor
 from datetime import datetime, timedelta
 
 
@@ -13,9 +14,9 @@ default_args = {
 }
 
 with DAG(
-    dag_id = 'exemplo1'
+    dag_id = 'exemplo1',
     start_date = datetime(2021, 1, 1),
-    schedule_interval='1 * * * *',
+    schedule_interval='*/2 * * * *',
     default_args = default_args,
     catchup = False
 ) as dag:
@@ -33,4 +34,10 @@ with DAG(
         http_conn_id = 'real_dolar_api',
         endpoint = 'last/USD-BRL',
         response_check = lambda response : True if check(response.status_code) is True else False
+    )
+
+    file_sensor = FileSensor(
+        task_id = 'file_sensor',
+        fs_conn_id = 'sensor_file',
+        filepath = 'teste-airflow.csv'
     )
